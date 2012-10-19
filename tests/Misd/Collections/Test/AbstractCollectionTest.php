f3df73f08046a7f4b12e4dd9d48166f5e45f5291
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-19 14:43:01 +0100

    Add missing license

--- tests/Misd/Collections/Test/AbstractCollectionTest.php
+++ tests/Misd/Collections/Test/AbstractCollectionTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Misd\Collections\Test;
 
 use PHPUnit_Framework_TestCase;
