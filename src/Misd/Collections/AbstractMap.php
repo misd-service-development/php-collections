e70e35ef9dc8d07e95c5992b6e9c78e48d3e004d
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-20 19:37:34 +0100

    Add abstract map and tests

---
+++ src/Misd/Collections/AbstractMap.php
@@ -0,0 +1,260 @@
+<?php
+
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\Collections;
+
+use Misd\Collections\Exception\UnsupportedOperationException;
+
+/**
+ * Abstract map.
+ *
+ * This provides a skeletal implementation of {@link MapInterface}, to
+ * minimize the effort required to implement this interface.
+ *
+ * This is an immutable map: you must overwrite methods such as `put()`
+ * to allow modification.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+abstract class AbstractMap implements MapInterface
+{
+    /**
+     * Values in this map.
+     *
+     * @var array
+     */
+    protected $values;
+
+    /**
+     * Keys in this map.
+     *
+     * @var array
+     */
+    protected $keys;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($map = array())
+    {
+        $this->values = array();
+        $this->keys = array();
+
+        if ($map instanceof MapInterface) {
+            $this->values = $map->values()->toArray();
+            $this->keys = $map->keySet()->toArray();
+        } else {
+            foreach ($map as $key => $value) {
+                $hash = $this->hashKey($key);
+                $this->values[$hash] = $value;
+                $this->keys[$hash] = $key;
+            }
+        }
+    }
+
+    /**
+     * Hash a key.
+     *
+     * @param mixed $key Key to be hashed.
+     *
+     * @return string|int Hashed key.
+     */
+    protected function hashKey($key)
+    {
+        if ($key instanceof \DateTime) {
+            return '_' . $key->format('c');
+        } elseif (is_object($key)) {
+            return '_' . spl_object_hash($key);
+        } elseif (false === $key) {
+            return '_false';
+        } elseif (true === $key) {
+            return '_true';
+        } elseif (null === $key) {
+            return '_null';
+        } elseif (is_array($key)) {
+            return '_' . md5(json_encode(array_multisort($key)));
+        } elseif (is_int($key)) {
+            return $key;
+        } else {
+            return (string)$key;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function put($key, $value)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function putAll($map)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key)
+    {
+        $hash = $this->hashKey($key);
+
+        return array_key_exists($hash, $this->values) ? $this->values[$hash] : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($key)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeAll($keys)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function containsKey($key)
+    {
+        return array_key_exists($this->hashKey($key), $this->values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function containsKeys($keys)
+    {
+        foreach ($keys as $key) {
+            if (false === $this->containsKey($key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function containsValue($value)
+    {
+        return in_array($value, $this->values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function containsValues($values)
+    {
+        foreach ($values as $value) {
+            if (false === $this->containsValue($value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function count()
+    {
+        return count($this->values());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEmpty()
+    {
+        return 0 === $this->count();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function keySet()
+    {
+        // TODO
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function values()
+    {
+        return new ArrayList($this->values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->values);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->values[] = $value;
+        } else {
+            $this->values[$offset] = $value;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->values[$offset]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->values[$offset]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->values[$offset]) ? $this->values[$offset] : null;
+    }
+}
---
+++ tests/Misd/Collections/Test/AbstractMapTest.php
@@ -0,0 +1,258 @@
+<?php
+
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\Collections\Test;
+
+use ReflectionMethod, DateTime;
+use PHPUnit_Framework_TestCase;
+use Misd\Collections\Test\Fixtures\TestObject;
+use Misd\Collections\ArrayList;
+
+class AbstractMapTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Misd\Collections\AbstractMap::__construct
+     */
+    public function testConstructorWithArray()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap', array(array('one', 'two')));
+        $this->assertEquals(array(0 => 'one', 1 => 'two'), $map->values()->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::__construct
+     */
+    public function testConstructorWithAssociativeArray()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one', 'two' => 'two'))
+        );
+        $this->assertEquals(array(0 => 'one', 1 => 'two'), $map->values()->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::hashKey
+     */
+    public function testHashKey()
+    {
+        $method = new \ReflectionMethod('Misd\Collections\AbstractMap', 'hashKey');
+        $method->setAccessible(true);
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $this->assertTrue(is_string($method->invoke($map, 'test')));
+        $this->assertTrue(is_int($method->invoke($map, 1)));
+        $this->assertTrue(is_string($method->invoke($map, new TestObject())));
+        $this->assertTrue(is_string($method->invoke($map, new DateTime())));
+        $this->assertTrue(is_string($method->invoke($map, false)));
+        $this->assertTrue(is_string($method->invoke($map, true)));
+        $this->assertTrue(is_string($method->invoke($map, null)));
+        $this->assertTrue(is_string($method->invoke($map, array('one', 'two'))));
+    }
+
+    /**
+     * @expectedException \Misd\Collections\Exception\UnsupportedOperationException
+     * @covers \Misd\Collections\AbstractMap::put
+     */
+    public function testPut()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $map->put('test', 'test');
+    }
+
+    /**
+     * @expectedException  \Misd\Collections\Exception\UnsupportedOperationException
+     * @covers \Misd\Collections\AbstractMap::putAll
+     */
+    public function testPutAll()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $map->putAll(array('one' => 'one', 'two' => 'two'));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::get
+     */
+    public function testGet()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertEquals(2, $map->get('two'));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::get
+     */
+    public function testGetNonExistent()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertNull($map->get('three'));
+    }
+
+    /**
+     * @expectedException  \Misd\Collections\Exception\UnsupportedOperationException
+     * @covers \Misd\Collections\AbstractMap::remove
+     */
+    public function testRemove()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $map->remove('test');
+    }
+
+    /**
+     * @expectedException  \Misd\Collections\Exception\UnsupportedOperationException
+     * @covers \Misd\Collections\AbstractMap::removeAll
+     */
+    public function testRemoveAll()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $map->removeAll(array('one', 'two'));
+    }
+
+    /**
+     * @expectedException  \Misd\Collections\Exception\UnsupportedOperationException
+     * @covers \Misd\Collections\AbstractMap::clear
+     */
+    public function testClear()
+    {
+        $map = $this->getMockForAbstractClass('Misd\Collections\AbstractMap');
+        $map->clear();
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsKey
+     */
+    public function testContainsKey()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertTrue($map->containsKey('two'));
+        $this->assertFalse($map->containsKey('three'));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsKeys
+     */
+    public function testContainsKeysWithArray()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertTrue($map->containsKeys(array('one', 'two')));
+        $this->assertFalse($map->containsKey(array('two', 'three')));
+        $this->assertFalse($map->containsKey(array('three', 'four')));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsKeys
+     */
+    public function testContainsKeysWithCollection()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertTrue($map->containsKeys(new ArrayList(array('one', 'two'))));
+        $this->assertFalse($map->containsKey(new ArrayList(array('two', 'three'))));
+        $this->assertFalse($map->containsKey(new ArrayList(array('three', 'four'))));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsValue
+     */
+    public function testContainsValue()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertTrue($map->containsValue(2));
+        $this->assertFalse($map->containsValue(3));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsValues
+     */
+    public function testContainsValuesWithArray()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 'two'))
+        );
+        $this->assertTrue($map->containsValues(array(1, 'two')));
+        $this->assertFalse($map->containsValues(array('two', 3)));
+        $this->assertFalse($map->containsValues(array(3, 4)));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::containsValues
+     */
+    public function testContainsValuesWithCollection()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 'two'))
+        );
+        $this->assertTrue($map->containsValues(new ArrayList(array(1, 'two'))));
+        $this->assertFalse($map->containsValues(new ArrayList(array('two', 3))));
+        $this->assertFalse($map->containsValues(new ArrayList(array(3, 4))));
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::count
+     */
+    public function testCount()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertEquals(2, $map->count());
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::isEmpty
+     */
+    public function testIsEmpty()
+    {
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap'
+        );
+        $this->assertTrue($map->isEmpty());
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 2))
+        );
+        $this->assertFalse($map->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\AbstractMap::values
+     */
+    public function testValues()
+    {
+        $object = new TestObject();
+        $map = $this->getMockForAbstractClass(
+            'Misd\Collections\AbstractMap',
+            array(array('one' => 1, 'two' => 'two', 2 => $object))
+        );
+
+        $this->assertInstanceOf('Misd\Collections\ListInterface', $map->values());
+        $this->assertEquals(array(1, 'two', $object), $map->values()->toArray());
+    }
+}
