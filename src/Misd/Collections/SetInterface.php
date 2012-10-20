8e9bca58c63a347ee7d53cf7380b017e4f075e4a
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-20 11:48:48 +0100

    Add SetInterface

---
+++ src/Misd/Collections/SetInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\Collections;
+
+/**
+ * Set.
+ *
+ * A collection that contains no duplicate elements.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+interface SetInterface extends CollectionInterface
+{
+}
