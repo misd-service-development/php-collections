a6de532655071a86260dfad08c4e705adeec244c
thewilkybarkid <chriswilkinson84@gmail.com>
2012-10-21 16:47:02 +0100

    Add hash set and tests

---
+++ src/Misd/Collections/HashSet.php
@@ -0,0 +1,182 @@
+<?php
+
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\Collections;
+
+/**
+ * Hash set.
+ *
+ * Hash-map-based implementation of {@link SetInterface}. It makes no
+ * guarantees as to the iteration order of the set; in particular, it does not
+ * guarantee that the order will remain constant over time. This class permits
+ * the null element.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+class HashSet extends AbstractSet
+{
+    /**
+     * @var HashMap
+     */
+    protected $elements;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($elements = array())
+    {
+        $this->elements = new HashMap();
+        $this->addAll($elements);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add($element)
+    {
+        $this->elements->put($element, $element);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAll($elements)
+    {
+        foreach ($elements as $element) {
+            $this->add($element);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function contains($element)
+    {
+        return $this->elements->containsValue($element);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function containsAll($elements)
+    {
+        foreach ($elements as $element) {
+            if (false === $this->elements->containsValue($element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($element)
+    {
+        $this->elements->remove($element);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeAll($elements)
+    {
+        $this->elements->removeAll($elements);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        $this->elements = new HashSet();
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function retainAll($elements)
+    {
+        // TODO
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        return $this->elements->values()->toArray();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->elements->values()->toArray());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        $elements = $this->elements->values();
+
+        if (is_null($offset)) {
+            $elements[] = $value;
+        } else {
+            $elements[$offset] = $value;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        $elements = $this->elements->values();
+
+        return isset($elements[$offset]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        $elements = $this->elements->values();
+
+        unset($elements[$offset]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        $elements = $this->elements->values();
+
+        return isset($elements[$offset]) ? $elements[$offset] : null;
+    }
+}
---
+++ tests/Misd/Collections/Test/HashSetTest.php
@@ -0,0 +1,296 @@
+<?php
+
+/*
+ * This file is part of the Collections library.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\Collections\Test;
+
+use PHPUnit_Framework_TestCase;
+use Misd\Collections\ArrayList,
+    Misd\Collections\HashSet;
+use Misd\Collections\Test\Fixtures\TestObject;
+
+/**
+ * Hash set test.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+class HashSetTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Misd\Collections\HashSet::__construct
+     * @covers \Misd\Collections\HashSet::toArray
+     */
+    public function testConstructorWithArray()
+    {
+        $set = new HashSet(array('one', 'two', 'two'));
+        $this->assertEquals(array('one', 'two'), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::__construct
+     */
+    public function testConstructorWithCollection()
+    {
+        $collection = new ArrayList(array('one', 'two', 'two'));
+        $set = new HashSet($collection);
+        $this->assertEquals(array('one', 'two'), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::add
+     */
+    public function testAdd()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $set->add('five');
+
+        $this->assertTrue($set->contains('five'));
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::add
+     */
+    public function testAddChaining()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->add('five'),
+            '->add() returns a reference to the set'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::addAll
+     */
+    public function testAddAllWithArray()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $set->addAll(array('five', 'six'));
+
+        $this->assertTrue($set->contains('five'));
+        $this->assertTrue($set->contains('six'));
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::addAll
+     */
+    public function testAddAllWithCollection()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+        $list = new ArrayList(array('five', 'six'));
+
+        $set->addAll($list);
+
+        $this->assertTrue($set->contains('five'));
+        $this->assertTrue($set->contains('six'));
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::addAll
+     */
+    public function testAddAllChaining()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->addAll(array('five', 'six')),
+            '->addAll() returns a reference to the list'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::contains
+     */
+    public function testContains()
+    {
+        $set = new HashSet(array('one', 'two'));
+        $this->assertTrue($set->contains('two'));
+        $this->assertFalse($set->contains('three'));
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::containsAll
+     */
+    public function testContainsAll()
+    {
+        $set = new HashSet(array('one', 'two'));
+        $this->assertTrue($set->containsAll(array('one', 'two')));
+        $this->assertFalse($set->containsAll(array('two', 'three')));
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::remove
+     */
+    public function testRemove()
+    {
+        $set = new HashSet(array('one', 'two'));
+
+        $set->remove('two');
+
+        $this->assertEquals(array('one'), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::remove
+     */
+    public function testRemoveNonexistent()
+    {
+        $set = new HashSet(array('one', 'two'));
+
+        $set->remove('three');
+
+        $this->assertEquals(array('one', 'two'), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::remove
+     */
+    public function testRemoveType()
+    {
+        $set = new HashSet(array(1, 2));
+
+        $set->remove(2);
+
+        $this->assertEquals(array(1), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::remove
+     */
+    public function testRemoveChaining()
+    {
+        $set = new HashSet(array('one', 'two'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->remove('two'),
+            '->remove() returns a reference to the set'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::removeAll
+     */
+    public function testRemoveAllWithArray()
+    {
+        $object1 = new \Misd\Collections\Test\Fixtures\TestObject();
+        $object1->firstValue = 'one';
+        $object2 = new \Misd\Collections\Test\Fixtures\TestObject();
+        $object2->secondValue = 'two';
+
+        $set = new HashSet(array(1, 'two', 'three', $object1, $object2));
+
+        $set->removeAll(array(1, 'three', 'four', $object1));
+
+        $this->assertEquals(array('two', $object2), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::removeAll
+     */
+    public function testRemoveAllWithSet()
+    {
+        $object1 = new \Misd\Collections\Test\Fixtures\TestObject();
+        $object1->firstValue = 'one';
+        $object2 = new \Misd\Collections\Test\Fixtures\TestObject();
+        $object2->secondValue = 'two';
+
+        $set = new HashSet(array(1, 'two', 'three', $object1, $object2));
+        $set2 = new HashSet(array(1, 'three', 'four', $object1));
+
+        $set->removeAll($set2);
+
+        $this->assertEquals(array('two', $object2), $set->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::removeAll
+     */
+    public function testRemoveAllChaining()
+    {
+        $set = new HashSet(array('one', 'two'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->removeAll(array('two')),
+            '->removeAll() returns a reference to the set'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::clear
+     */
+    public function testClear()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $set->clear();
+
+        $this->assertEquals(0, $set->count());
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::clear
+     */
+    public function testClearChaining()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->clear(),
+            '->clear() returns a reference to the set'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::retainAll
+     */
+    public function testRetainAllWithArray()
+    {
+        // TODO
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::retainAll
+     */
+    public function testRetainAllWithSet()
+    {
+        // TODO
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::retainAll
+     */
+    public function testRetainAllChaining()
+    {
+        $set = new HashSet(array('one', 'two', 'three', 'four'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\SetInterface',
+            $set->retainAll(array('two', 'three')),
+            '->retainAll() returns a reference to the set'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\HashSet::getIterator
+     */
+    public function testGetIterator()
+    {
+        $set = new HashSet();
+        $this->assertInstanceOf('Traversable', $set->getIterator());
+    }
+}
