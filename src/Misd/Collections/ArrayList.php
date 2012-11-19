ee56a65ac1b68a1440f302c3491486617a266c07
thewilkybarkid <chriswilkinson84@gmail.com>
2012-11-19 20:17:09 +0000

    Add in ArrayList::subList() and tests

--- src/Misd/Collections/ArrayList.php
+++ src/Misd/Collections/ArrayList.php
@@ -24,6 +24,11 @@ use OutOfBoundsException;
 class ArrayList extends AbstractList
 {
     /**
+     * @var SubArrayList[]
+     */
+    protected $subLists = array();
+
+    /**
      * Adds the element to the end of the list.
      *
      * @param mixed $element Element to add to the list.
@@ -84,6 +89,10 @@ class ArrayList extends AbstractList
 
         array_splice($this->elements, $index, 0, $element);
 
+        foreach ($this->subLists as $subList) {
+            $subList->insert($index, $element, true);
+        }
+
         return $this;
     }
 
@@ -118,6 +127,10 @@ class ArrayList extends AbstractList
 
         array_splice($this->elements, $index, 0, $elements);
 
+        foreach ($this->subLists as $subList) {
+            $subList->insertAll($index, $elements, true);
+        }
+
         return $this;
     }
 
@@ -140,6 +153,10 @@ class ArrayList extends AbstractList
 
         $this->elements[$index] = $element;
 
+        foreach ($this->subLists as $subList) {
+            $subList->set($index, $element, true);
+        }
+
         return $this;
     }
 
@@ -159,6 +176,10 @@ class ArrayList extends AbstractList
         if (false !== $key) {
             unset($this->elements[$key]);
             $this->elements = array_values($this->elements);
+        }
+
+        foreach ($this->subLists as $subList) {
+            $subList->drop($key, true);
         }
 
         return $this;
@@ -199,6 +220,10 @@ class ArrayList extends AbstractList
             )
         );
 
+        foreach ($this->subLists as $subList) {
+            $subList->removeAll($elements, true);
+        }
+
         return $this;
     }
 
@@ -223,6 +248,10 @@ class ArrayList extends AbstractList
         unset($this->elements[$index]);
         $this->elements = array_values($this->elements);
 
+        foreach ($this->subLists as $subList) {
+            $subList->drop($index, true);
+        }
+
         return $this;
     }
 
@@ -234,6 +263,10 @@ class ArrayList extends AbstractList
     public function clear()
     {
         $this->elements = array();
+
+        foreach ($this->subLists as $subList) {
+            $subList->clear(true);
+        }
 
         return $this;
     }
@@ -274,6 +307,183 @@ class ArrayList extends AbstractList
                 }
             )
         );
+
+        return $this;
+    }
+
+    /**
+     * Returns a view of the portion of this list between the specified
+     * `fromIndex`, inclusive, and `toIndex`, exclusive. (If `fromIndex` and
+     * `toIndex` are equal, the returned list is empty.)
+     *
+     * The returned list is backed by this list, so changes in the returned
+     * list are reflected in this list, and vice-versa. The returned list
+     * supports all of the optional list operations supported by this list.
+     *
+     * @param int $fromIndex Low endpoint (inclusive) of the sub-list.
+     * @param int $toIndex   High endpoint (exclusive) of the sub-list.
+     *
+     * @return ArrayList A view of the specified range within this list.
+     *
+     * @throws OutOfBoundsException If one of the indices is out of range.
+     */
+    public function subList($fromIndex, $toIndex)
+    {
+        if ($fromIndex > $toIndex || $fromIndex < 0 || $toIndex > $this->count()) {
+            throw new OutOfBoundsException();
+        }
+
+        $subList = SubArrayList::factory($this, $fromIndex, $toIndex);
+        $this->subLists[] = $subList;
+
+        return $subList;
+    }
+}
+
+/**
+ * Array list that is a section of a parent array list. Do not create this
+ * object anywhere other than in `ArrayList::subList()`.
+ *
+ * @ignore
+ * @internal
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+final class SubArrayList extends ArrayList
+{
+    /**
+     * @var ArrayList
+     */
+    private $parent;
+    private $fromIndex;
+
+    public static function factory(ArrayList $parent, $fromIndex, $toIndex)
+    {
+        $subList = new self(array_values(
+            array_slice($parent->toArray(), $fromIndex, $toIndex - $fromIndex, true)
+        ));
+        $subList->parent = $parent;
+        $subList->fromIndex = $fromIndex;
+
+        return $subList;
+    }
+
+    public function add($element)
+    {
+        $this->parent->insert($this->fromIndex + $this->count(), $element);
+
+        return $this;
+    }
+
+    public function addAll($elements)
+    {
+        $this->parent->insertAll($this->fromIndex + $this->count(), $elements);
+
+        return $this;
+    }
+
+    public function insert($index, $element, $fromParent = false)
+    {
+        if (true === $fromParent) {
+            $thisIndex = $index - $this->fromIndex;
+            if (0 <= $thisIndex && $this->count() >= $thisIndex) {
+                parent::insert($thisIndex, $element);
+            }
+        } else {
+            $this->parent->insert($this->fromIndex + $index, $element);
+        }
+
+        return $this;
+    }
+
+    public function insertAll($index, $elements, $fromParent = false)
+    {
+        if (true === $fromParent) {
+            $thisIndex = $index - $this->fromIndex;
+            if (0 <= $thisIndex && $this->count() >= $thisIndex) {
+                parent::insertAll($thisIndex, $elements);
+            }
+        } else {
+            $this->parent->insertAll($this->fromIndex + $index, $elements);
+        }
+
+        return $this;
+    }
+
+    public function set($index, $element, $fromParent = false)
+    {
+        if (true === $fromParent) {
+            $thisIndex = $index - $this->fromIndex;
+            if (0 <= $thisIndex && $this->count() > $thisIndex) {
+                parent::set($thisIndex, $element);
+            }
+        } else {
+            $this->parent->set($index + $this->fromIndex, $element);
+        }
+
+        return $this;
+    }
+
+    public function get($index)
+    {
+        if (0 > $index && $this->count() > $index) {
+            throw new OutOfBoundsException($index);
+        }
+
+        return $this->parent->get($index + $this->fromIndex);
+    }
+
+    public function remove($element)
+    {
+        $key = array_search($element, $this->toArray(), true);
+
+        if (false !== $key) {
+            $this->parent->drop($key + $this->fromIndex);
+        }
+
+        return $this;
+    }
+
+    public function removeAll($elements, $fromParent = false)
+    {
+        if (true === $fromParent) {
+            parent::removeAll($elements);
+        } else {
+            $this->parent->removeAll($elements);
+        }
+
+        return $this;
+    }
+
+    public function drop($index, $fromParent = false)
+    {
+        if (true === $fromParent) {
+            $thisIndex = $index - $this->fromIndex;
+            if (0 <= $thisIndex && $this->count() > $thisIndex) {
+                parent::drop($thisIndex);
+            }
+        } else {
+            $this->parent->drop($index + $this->fromIndex);
+        }
+
+        return $this;
+    }
+
+    public function retainAll($elements)
+    {
+        // TODO
+    }
+
+    public function clear($fromParent = false)
+    {
+        if (true === $fromParent) {
+            parent::clear();
+        } else {
+            $loops = $this->count();
+            for ($i = 0; $i < $loops; $i++) {
+                $this->parent->drop($this->fromIndex);
+            }
+        }
 
         return $this;
     }
--- tests/Misd/Collections/Test/ArrayListTest.php
+++ tests/Misd/Collections/Test/ArrayListTest.php
@@ -433,4 +433,468 @@ class ArrayListTest extends PHPUnit_Framework_TestCase
             '->retainAll() returns a reference to the list'
         );
     }
+
+    /**
+     * @covers \Misd\Collections\ArrayList::subList
+     * @covers \Misd\Collections\SubArrayList::factory
+     */
+    public function testSubList()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four'));
+
+        $this->assertInstanceOf('Misd\Collections\ListInterface', $list->subList(0, 2));
+
+        $emptySubList = $list->subList(0, 0);
+        $this->assertTrue($emptySubList->isEmpty());
+
+        $subList = $list->subList(0, 2);
+        $this->assertEquals(array('one', 'two'), $subList->toArray());
+
+        $subSubList = $list->subList(1, 2);
+        $this->assertEquals(array('two'), $subSubList->toArray());
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @covers \Misd\Collections\ArrayList::subList
+     */
+    public function testSubListOutOfBoundsExceptionOnNegativeFromIndex()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four'));
+
+        $list->subList(-1, 2);
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @covers \Misd\Collections\ArrayList::subList
+     */
+    public function testSubListOutOfBoundsExceptionOnReversedIndices()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four'));
+
+        $list->subList(1, 0);
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @covers \Misd\Collections\ArrayList::subList
+     */
+    public function testSubListOutOfBoundsExceptionOnNonExistentToIndex()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four'));
+
+        $list->subList(2, 5);
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::add
+     */
+    public function testSubListAdd()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 3);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->add('list');
+        $subList->add('subList');
+        $subSubList->add('subSubList');
+
+        $this->assertEquals(
+            array('one', 'two', 'subSubList', 'three', 'subList', 'four', 'five', 'list'),
+            $list->toArray()
+        );
+        $this->assertEquals(array('two', 'subSubList', 'three', 'subList'), $subList->toArray());
+        $this->assertEquals(array('two', 'subSubList'), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::addAll
+     */
+    public function testSubListAddAll()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 3);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->addAll(array('list'));
+        $subList->addAll(array('subList'));
+        $subSubList->addAll(array('subSubList'));
+
+        $this->assertEquals(
+            array('one', 'two', 'subSubList', 'three', 'subList', 'four', 'five', 'list'),
+            $list->toArray()
+        );
+        $this->assertEquals(array('two', 'subSubList', 'three', 'subList'), $subList->toArray());
+        $this->assertEquals(array('two', 'subSubList'), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::insert
+     * @covers \Misd\Collections\ArrayList::insert
+     */
+    public function testSubListInsert()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 3);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->insert(3, 'list');
+        $subList->insert(2, 'subList');
+        $subSubList->insert(1, 'subSubList');
+
+        $this->assertEquals(
+            array('one', 'two', 'subSubList', 'three', 'subList', 'list', 'four', 'five'),
+            $list->toArray()
+        );
+        $this->assertEquals(array('two', 'subSubList', 'three', 'subList', 'list'), $subList->toArray());
+        $this->assertEquals(array('two', 'subSubList'), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::insertAll
+     * @covers \Misd\Collections\ArrayList::insertAll
+     */
+    public function testSubListInsertAll()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 3);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->insertAll(3, array('list'));
+        $subList->insertAll(2, array('subList'));
+        $subSubList->insertAll(1, array('subSubList'));
+
+        $this->assertEquals(
+            array('one', 'two', 'subSubList', 'three', 'subList', 'list', 'four', 'five'),
+            $list->toArray()
+        );
+        $this->assertEquals(array('two', 'subSubList', 'three', 'subList', 'list'), $subList->toArray());
+        $this->assertEquals(array('two', 'subSubList'), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::set
+     * @covers \Misd\Collections\ArrayList::set
+     */
+    public function testSubListSet()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->set(2, 'list');
+        $subList->set(2, 'subList');
+        $subSubList->set(0, 'subSubList');
+
+        $this->assertEquals(
+            array('one', 'subSubList', 'list', 'subList', 'five'),
+            $list->toArray()
+        );
+        $this->assertEquals(array('subSubList', 'list', 'subList'), $subList->toArray());
+        $this->assertEquals(array('subSubList'), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::get
+     */
+    public function testSubListGet()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $this->assertEquals('three', $subList->get(1));
+        $this->assertEquals('two', $subSubList->get(0));
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @covers \Misd\Collections\SubArrayList::get
+     */
+    public function testSubListGetOutOfBoundsExceptionOnNegativeIndex()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+
+        $subList->get(-1);
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     * @covers \Misd\Collections\SubArrayList::get
+     */
+    public function testSubListGetOutOfBoundsExceptionOnNonExistentIndex()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+
+        $subList->get(4);
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::remove
+     */
+    public function testSubListRemove()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 2);
+
+        $list->remove('two');
+        $subList->remove('four');
+        $subSubList->remove('three');
+
+        $this->assertEquals(array('one', 'five'), $list->toArray());
+        $this->assertTrue($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::remove
+     */
+    public function testSubListRemoveNonexistent()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+
+        $subList->remove('five');
+
+        $this->assertEquals(array('one', 'two', 'three', 'four', 'five'), $list->toArray());
+        $this->assertEquals(array('two', 'three', 'four'), $subList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::remove
+     */
+    public function testSubListRemoveType()
+    {
+        $list = new ArrayList(array(1, '2', 2, '2', 2));
+        $subList = $list->subList(2, 5);
+
+        $subList->remove(2);
+
+        $this->assertEquals(array(1, '2', '2', 2), $list->toArray());
+        $this->assertEquals(array('2', 2), $subList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::remove
+     */
+    public function testSubListRemoveChaining()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+
+        $this->assertInstanceOf(
+            'Misd\Collections\ListInterface',
+            $subList->remove('two'),
+            '->remove() returns a reference to the list'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::removeAll
+     */
+    public function testSubListRemoveAllWithArray()
+    {
+        $object1 = new TestObject();
+        $object1->firstValue = 'one';
+        $object2 = new TestObject();
+        $object2->secondValue = 'two';
+
+        $list = new ArrayList(array('1', 'two', 'two', 'three', 'three', 'three', $object1, $object2));
+        $subList = $list->subList(2, 7);
+        $subSubList = $subList->subList(2, 4);
+
+        $list->removeAll(array(1, 'three', 'four', $object1));
+
+        $this->assertEquals(array('1', 'two', 'two', $object2), $list->toArray());
+        $this->assertEquals(array('two'), $subList->toArray());
+        $this->assertTrue($subSubList->isEmpty());
+
+        $list = new ArrayList(array('1', 'two', 'two', 'three', 'three', 'three', $object1, $object2));
+        $subList = $list->subList(2, 7);
+        $subSubList = $subList->subList(2, 4);
+
+        $subList->removeAll(array('two', $object1));
+        $subSubList->removeAll(array('three'));
+
+        $this->assertEquals(array('1', $object2), $list->toArray());
+        $this->assertTrue($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::removeAll
+     */
+    public function testSubListRemoveAllWithList()
+    {
+        $object1 = new TestObject();
+        $object1->firstValue = 'one';
+        $object2 = new TestObject();
+        $object2->secondValue = 'two';
+
+        $list = new ArrayList(array('1', 'two', 'two', 'three', 'three', 'three', $object1, $object2));
+        $subList = $list->subList(2, 7);
+        $subSubList = $subList->subList(2, 4);
+
+        $list->removeAll(new ArrayList(array(1, 'three', 'four', $object1)));
+
+        $this->assertEquals(array('1', 'two', 'two', $object2), $list->toArray());
+        $this->assertEquals(array('two'), $subList->toArray());
+        $this->assertTrue($subSubList->isEmpty());
+
+        $list = new ArrayList(array('1', 'two', 'two', 'three', 'three', 'three', $object1, $object2));
+        $subList = $list->subList(2, 7);
+        $subSubList = $subList->subList(2, 4);
+
+        $subList->removeAll(new ArrayList(array('two', $object1)));
+        $subSubList->removeAll(new ArrayList(array('three')));
+
+        $this->assertEquals(array('1', $object2), $list->toArray());
+        $this->assertTrue($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::removeAll
+     */
+    public function testSubListRemoveAllChaining()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+
+        $this->assertInstanceOf(
+            'Misd\Collections\ListInterface',
+            $subList->removeAll(array('two')),
+            '->removeAll() returns a reference to the list'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::drop
+     * @covers \Misd\Collections\ArrayList::drop
+     */
+    public function testSubListDrop()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->drop(2);
+        $subList->drop(1);
+        $subSubList->drop(0);
+
+        $this->assertEquals(
+            array('one', 'five'),
+            $list->toArray()
+        );
+        $this->assertEquals(array(), $subList->toArray());
+        $this->assertEquals(array(), $subSubList->toArray());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::clear
+     */
+    public function testSubListClear()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $list->clear();
+
+        $this->assertTrue($list->isEmpty());
+        $this->assertTrue($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $subList->clear();
+
+        $this->assertEquals(array('one', 'five'), $list->toArray());
+        $this->assertTrue($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 1);
+
+        $subSubList->clear();
+
+        $this->assertEquals(array('one', 'three', 'four', 'five'), $list->toArray());
+        $this->assertEquals(array('three', 'four'), $subList->toArray());
+        $this->assertTrue($subSubList->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::clear
+     */
+    public function testSubListClearChaining()
+    {
+        $list = new ArrayList(array('one', 'two', 'two'));
+
+        $this->assertInstanceOf(
+            'Misd\Collections\ListInterface',
+            $list->removeAll(array('two')),
+            '->removeAll() returns a reference to the list'
+        );
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::count
+     */
+    public function testSubListCount()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 2);
+
+        $this->assertEquals(5, $list->count());
+        $this->assertEquals(3, $subList->count());
+        $this->assertEquals(2, $subSubList->count());
+
+        $subSubList->clear();
+
+        $this->assertEquals(3, $list->count());
+        $this->assertEquals(1, $subList->count());
+        $this->assertEquals(0, $subSubList->count());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::isEmpty
+     */
+    public function testSubListIsEmpty()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(0, 2);
+
+        $this->assertFalse($list->isEmpty());
+        $this->assertFalse($subList->isEmpty());
+        $this->assertFalse($subSubList->isEmpty());
+
+        $subSubList->clear();
+
+        $this->assertFalse($list->isEmpty());
+        $this->assertFalse($subList->isEmpty());
+        $this->assertTrue($subSubList->isEmpty());
+    }
+
+    /**
+     * @covers \Misd\Collections\SubArrayList::toArray
+     */
+    public function testSubListToArray()
+    {
+        $list = new ArrayList(array('one', 'two', 'three', 'four', 'five'));
+        $subList = $list->subList(1, 4);
+        $subSubList = $subList->subList(1, 2);
+
+        $this->assertEquals(array(0 => 'one', 1 => 'two', 2 => 'three', 3 => 'four', 4 => 'five'), $list->toArray());
+        $this->assertEquals(array(0 => 'two', 1 => 'three', 2 => 'four'), $subList->toArray());
+        $this->assertEquals(array(0 => 'three'), $subSubList->toArray());
+    }
 }
